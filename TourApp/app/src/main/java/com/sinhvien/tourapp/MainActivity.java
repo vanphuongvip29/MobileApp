e355d205af2c125b116df96353873828e1bd7756
vanphuongvip29 <vanphuongvip29@gmail.com>
2023-07-02 22:44:49 +0700

    MenuBottom-Fragment

--- TourApp/app/src/main/java/com/sinhvien/tourapp/MainActivity.java
+++ TourApp/app/src/main/java/com/sinhvien/tourapp/MainActivity.java
@@ -1,14 +1,55 @@
 package com.sinhvien.tourapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    BillFragment billFragment = new BillFragment();
+    AccountFragment accountFragment = new AccountFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.action_home)
+                {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.action_shopping) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, billFragment).commit();
+                    return true;
+                }
+                else {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, accountFragment).commit();
+                    return true;
+                }
+            }
+        });
     }
+
+
 }
